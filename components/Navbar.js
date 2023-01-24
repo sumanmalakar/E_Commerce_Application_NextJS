3c289ffa2f0f422a261886423b2d561c75318462
Suman Malakar <malakars041@gmail.com>
2023-01-24 22:08:31 +0530

    adding the product and start working on the backand api

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-no-undef */
-import React, {useRef} from 'react'
-import Link from 'next/link';
-
+import React, { useRef } from "react";
+import Link from "next/link";
 
 import {
   AiOutlineShoppingCart,
@@ -12,29 +11,20 @@ import {
 
 import { BsFillBagCheckFill } from "react-icons/bs";
 
-
-
-
-
-
 export default function Navbar() {
-const toggleCart = () =>{
-console.log("Cart is clicked")
+  const toggleCart = () => {
+    console.log("Cart is clicked");
 
-if(ref.current.classList.contains('translate-x-full')){
-  ref.current.classList.remove('translate-x-full')
-  ref.current.classList.add('translate-x-0')
-}
+    if (ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-full");
+      ref.current.classList.add("translate-x-0");
+    } else if (!ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-0");
+      ref.current.classList.add("translate-x-full");
+    }
+  };
 
-else if(!ref.current.classList.contains('translate-x-full')){
-  ref.current.classList.remove('translate-x-0')
-  ref.current.classList.add('translate-x-full')
-}
-
-
-}
-
-const ref  = useRef()
+  const ref = useRef();
 
   return (
     <div className="flex flex-col md:flex-row md:justify-start justify-center items-center bg-pink-600 text-white">
@@ -81,7 +71,7 @@ const ref  = useRef()
 
       <div
         ref={ref}
-        className="w-72  sideCart absolute top-0 right-0 bg-pink-100 px-8 py-10 text-black 
+        className="w-72 h-full sideCart absolute top-0 right-0 bg-pink-100 px-8 py-10 text-black 
         transform
          transition-transform translate-x-full"
       >
@@ -93,16 +83,13 @@ const ref  = useRef()
           <AiFillCloseCircle />
         </span>
         <ol className="list-decimal font-semibold py-2">
-
-
-          
           <li>
             <div className="item flex my-5">
               <div className="w-2/3 font-semibold">Tshirt - Wear the code</div>
               <div className="flex justify-center items-center w-1/3 font-semibold text-lg">
-                <AiFillMinusCircle className="text-pink-500 cursor-pointer" /> 
+                <AiFillMinusCircle className="text-pink-500 cursor-pointer" />
                 <span className="mx-3 text-sm">1</span>
-                <AiFillPlusCircle  className="text-pink-500 cursor-pointer" />
+                <AiFillPlusCircle className="text-pink-500 cursor-pointer" />
               </div>
             </div>
           </li>
@@ -111,9 +98,9 @@ const ref  = useRef()
             <div className="item flex my-5">
               <div className="w-2/3 font-semibold">Tshirt - Wear the code</div>
               <div className="flex justify-center items-center w-1/3 font-semibold text-lg">
-                <AiFillMinusCircle className="text-pink-500 cursor-pointer" /> 
+                <AiFillMinusCircle className="text-pink-500 cursor-pointer" />
                 <span className="mx-3 text-sm">1</span>
-                <AiFillPlusCircle  className="text-pink-500 cursor-pointer" />
+                <AiFillPlusCircle className="text-pink-500 cursor-pointer" />
               </div>
             </div>
           </li>
@@ -122,9 +109,9 @@ const ref  = useRef()
             <div className="item flex my-5">
               <div className="w-2/3 font-semibold">Tshirt - Wear the code</div>
               <div className="flex justify-center items-center w-1/3 font-semibold text-lg">
-                <AiFillMinusCircle className="text-pink-500 cursor-pointer" /> 
+                <AiFillMinusCircle className="text-pink-500 cursor-pointer" />
                 <span className="mx-3 text-sm">1</span>
-                <AiFillPlusCircle  className="text-pink-500 cursor-pointer" />
+                <AiFillPlusCircle className="text-pink-500 cursor-pointer" />
               </div>
             </div>
           </li>
@@ -133,21 +120,24 @@ const ref  = useRef()
             <div className="item flex my-5">
               <div className="w-2/3 font-semibold">Tshirt - Wear the code</div>
               <div className="flex justify-center items-center w-1/3 font-semibold text-lg">
-                <AiFillMinusCircle className="text-pink-500 cursor-pointer" /> 
+                <AiFillMinusCircle className="text-pink-500 cursor-pointer" />
                 <span className="mx-3 text-sm">1</span>
-                <AiFillPlusCircle  className="text-pink-500 cursor-pointer" />
+                <AiFillPlusCircle className="text-pink-500 cursor-pointer" />
               </div>
             </div>
           </li>
-       
-         
-    
         </ol>
-        <button class="flex text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-sm">
-          <BsFillBagCheckFill className="m-1" /> 
-        CheckOut</button>
-      
-      
+
+        <div className="flex">
+          <button class="flex text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm mx-2">
+            <BsFillBagCheckFill className="m-1" />
+            CheckOut
+          </button>
+
+          <button class="flex text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm mx-2">
+            Clear Cart
+          </button>
+        </div>
       </div>
     </div>
   );
