f4d177fac0b605adb3df5dc3db0bb8ba7a373fcc
Suman Malakar <malakars041@gmail.com>
2023-01-31 14:28:25 +0530

    implementing the serverside props in tshirts.js paga

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,201 +1,63 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import Product from "../models/Product";
+import mongoose from "mongoose";
 
-export default function tshirts() {
+export default function tshirts({ products }) {
+  console.log(products);
+
   return (
     <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap mt-4 justify-center">
+            {products.map((item) => {
+              return (
+                <>
+                  <div
+                    key={item._id}
+                    class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5"
+                  >
+                    <Link
+                      href={`/product/${item.slug}`}
+                      class="block relative  rounded overflow-hidden "
+                    >
+                      <img
+                        alt="ecommerce"
+                        class="m-auto   h-[30vh] md:h-[36h] block"
+                        src={item.img}
+                      />
+                    <div class="mt-4 text-center">
+                      <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                        T-Shirts
+                      </h3>
+                      <h2 class="text-gray-900 title-font text-lg font-medium">
+                        {item.title}
+                      </h2>
+                      <p class="mt-1">{item.price}</p>
+                      <p class="mt-1">S, M, L, XL, XLL</p>
+                    </div>
+                    </Link>
 
-      <section class="text-gray-600 body-font">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-wrap -m-4 justify-center">
-
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-            <div class="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-5 ">
-              <Link href={'/product/wear-the-code'} class="block relative  rounded overflow-hidden ">
-                <img alt="ecommerce" class="m-auto   h-[30vh] md:h-[36h] block" src="https://m.media-amazon.com/images/I/51JE7Y0SFmL._UY741_.jpg" />
-              </Link>
-              <div class="mt-4 text-center">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The E-commerce</h2>
-                <p class="mt-1">₹499.00</p>
-                <p class="mt-1">S, M, L, XL, XLL</p>
-              </div>
-            </div>
-
-
-
-
-
+                  </div>
+                </>
+              );
+            })}
           </div>
         </div>
       </section>
-
-      
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+  let products = await Product.find({ category: 'tshirts' });
+  // console.log(products)
+  return {
+    props: { products: JSON.parse(JSON.stringify(products)) }, // will be passed to the page component as props
+  };
 }
