b291a294ef126bf954321318dfd039a37d070359
Suman Malakar <malakars041@gmail.com>
2023-02-09 19:54:45 +0530

    adding the crypto-js password encrption

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,13 +1,21 @@
 
 import User from "../../models/User";
 import connectDb from "../../middleware/mongoose";
+var CryptoJS = require("crypto-js");
+
 
 const handler = async (req, res) => {
   if (req.method == 'POST') {
     console.log(req.body)
     let user = await User.findOne({ "email": req.body.email })
+
+    const bytes = CryptoJS.AES.decrypt(user.password, 'secret123');
+    let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+
     if (user) {
-      if (req.body.email == user.email && req.body.password == user.password) {
+      // console.log(user.password)
+      // console.log(decryptedData)
+      if (req.body.email == user.email && req.body.password == decryptedData) {
         res.status(200).json({ success: true, eamil: user.email, name: user.name })
       } else {
         res.status(200).json({ success: false, error: "Invailid Credentials" })
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,10 +2,20 @@
 
 import User from "../../models/User";
 import connectDb from "../../middleware/mongoose";
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
   if (req.method == 'POST') {
     console.log(req.body)
+
+    // const {name, email} = req.body;
+    
+    // let u = new User({ name, email, password: CryptoJS.AES.encrypt(req.body.password, "secret123").toString() });
+
+    // await u.save()
+    // res.status(200).json({ success: true, name: u.name, email: u.email })
+
+
     const user = await User.findOne({ email: req.body.email });
 
     if (req.body.name === '' || req.body.email === '' || req.body.password === '') {
@@ -14,11 +24,18 @@ const handler = async (req, res) => {
       res.status(200).json({ userExist: true, msg: "User already exist..!" })
     }
     else {
-      let u = new User(req.body);
-      await u.save();
 
+      const { name, email } = req.body;
+
+      let u = new User({ name, email, password: CryptoJS.AES.encrypt(req.body.password, "secret123").toString() });
+
+      await u.save()
       res.status(200).json({ success: true, name: u.name, email: u.email })
+
+ 
     }
+
+
 
   } else {
     res.status(400).json({ error: "This method is not allowed" })
